cf0e5f6b1ed2cbb171259f7983f3bce28300e1d3
theSLWayne <hiranwayne@yahoo.com>
2019-10-04 04:09:47 +0530

    Article list stylize

--- backend/models.py
+++ backend/models.py
@@ -13,6 +13,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length = 100, help_text = "Enter ingredient name.")
     manufacturer = models.CharField(max_length = 100, help_text = "Enter manufacturer.")
     price = models.FloatField(help_text = "Enter price.")
+    photo = models.ImageField(upload_to="gallery", null = True)
 
     def __str__(self):
         return self.name
@@ -26,6 +27,8 @@ class Article(models.Model):
     description = models.TextField(help_text = "Enter description")
     ingredients = models.ManyToManyField(Ingredient, help_text = "Enter ingredients.")
     created_on = models.DateField(help_text = "Enter created date")
+    photo = models.ImageField(upload_to="gallery", null = True)
+
 
     def __str__(self):
         return self.title
--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,6 @@ urlpatterns = [
 urlpatterns += [
     path('backend/', include('backend.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
