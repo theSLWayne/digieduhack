f8b858f901d7dd6af750beb01a58f9ec82d2b8e8
theSLWayne <hiranwayne@yahoo.com>
2019-10-03 23:18:50 +0530

    Added Navbar

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('articles/', views.ArticleListView.as_view(), name = 'articles'),
